54a3c27b9d7db320bc1d3e4ea136ed5f337d5db7
knighterzz <56337946+knighterzz@users.noreply.github.com>
2021-07-09 20:47:47 +0700

    Add files via upload

---
+++ Beranda/index.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" class="">
+	<link rel="shortcut icon" type="image/x-icon" href="Group 1 (1).png" />
+    <script src="https://kit.fontawesome.com/c424c66699.js" crossorigin="anonymous"></script>
+    <title>Fundamental</title>
+  </head>
+  <body>
+  
+   <!-- Image and text -->
+        <nav class="navbar navbar-dark bg-primary fixed-top">
+          <a class="navbar-brand" href="#">
+            <img src="Group 1 (2).png" width="30" height="30" class="d-inline-block align-top" alt="">
+            <b>Fundamental</b>
+          </a>
+        </nav>
+    <!--jumpbotron-->
+        <div class="row"></div>
+          <div class="jumbotron">
+            <div class="container text-white">
+                <h1 class="display-4">Grow up your coffee shop with <br><span class="font-weight-bold">FUNDAMENTAL SYSTEM</span></h1>
+                <hr class="my-4">
+                <p class="lead">Information System For Your Coffee Shop </p>
+                <a class="btn btn-primary btn-lg mr-2" href="../Fundamental/login.php" role="button">Masuk</a>
+                <a class="btn btn-primary btn-lg" href="../Fundamental/register.php" role="button">Daftar</a>
+            </div>
+          </div>
+        
+  <section id="services" class="container">
+   <h2 class="display-4 text-center mt-5 mb-4">We Can Handle</h2>
+   <div class="row text-center">
+      <div class="col-md-4 mb-4">
+         <div class="card h-100">
+            <img class="card-img-top" src="https://www.jurnal.id/wp-content/uploads/2019/01/shutterstock_686660953-1.jpg" alt="">
+            <div class="card-body">
+               <h4 class="card-title">Kalkulasi</h4>
+               <p class="card-text">Fundamental merupakan sistem informasi yang mampu melakukan kalkulasi untung dan rugi</p>
+            </div>
+         </div>
+      </div>
+      
+      <div class="col-md-4 mb-4">
+         <div class="card h-100">
+            <img class="card-img-top" src="https://www.translatemedia.com/wp-content/uploads/2017/10/Untitled-design-4.jpg" alt="Development">
+            <div class="card-body">
+               <h4 class="card-title">Keputusan</h4>
+                  <p class="card-text">Fundamental dapat membantu membantu menampilkan data untuk menunjang mengambil keputusan</p>
+            </div>
+         </div>
+      </div>
+      
+      <div class="col-md-4 mb-4">
+         <div class="card h-100">
+            <img class="card-img-top" src="http://www.integragen.com/wp-content/uploads/integragen-company-annual-financial-reports.jpg" alt="Analytics">
+            <div class="card-body">
+               <h4 class="card-title">Laporan</h4>
+               <p class="card-text">Fundamental dapat membantu menyajikan laporan yang di inginkan</p>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+    <div class="contact-form">
+    <div class="container">
+      <form>
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-12">
+          <h1>HUBUNGI KAMI SEGERA</h1> 
+        </div>
+        <div class="col-lg-8 col-md-8 col-sm-12 right">
+          <div class="form-group">
+            <input type="text" class="form-control form-control-lg" placeholder="Your Name" name="">
+          </div>
+          <div class="form-group">
+            <input type="email" class="form-control form-control-lg" placeholder="YourEmail@email.com" name="email">
+          </div>
+          <div class="form-group">
+            <textarea class="form-control form-control-lg">
+              
+            </textarea>
+          </div>
+          <input type="submit" class="btn btn-secondary btn-block rounded" value="Send" name="">
+        </div>
+      </div>
+      </form>
+    </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+  <footer class="py-3 bg-light">
+      <div class="container">
+          <p class="text-center">Copyright &copy; NextAjent Company</p>
+      </div>
+    </footer>
+</html>
